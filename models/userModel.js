a5942d9858b6c3b04a7739aa2471a87ff9cde444
TAR22Q <steamk77@gmail.com>
2024-08-04 13:47:11 +0300

    get_user

--- models/userModel.js
+++ models/userModel.js
@@ -13,7 +13,7 @@ exports.getAllUsers = async () => {
 
 exports.getUserById = async (id) => {
   try {
-    const result = await client.query("SELECT * FROM users WHERE id = $1", [
+    const result = await client.query("SELECT * FROM get_user WHERE id = $1", [
       id,
     ]);
     const user = result.rows[0];
@@ -26,7 +26,7 @@ exports.getUserById = async (id) => {
 exports.getUserByName = async (username) => {
   try {
     const result = await client.query(
-      "SELECT * FROM users WHERE username LIKE $1",
+      "SELECT * FROM get_user WHERE username LIKE $1",
       [username]
     );
     const user = result.rows[0];
