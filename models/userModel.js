7b0f9d24f9afeb387e4a1134bac75c4eb1f7a886
TAR22Q <steamk77@gmail.com>
2024-08-04 12:55:48 +0300

    fixing user login

--- models/userModel.js
+++ models/userModel.js
@@ -13,7 +13,7 @@ exports.getAllUsers = async () => {
 
 exports.getUserById = async (id) => {
   try {
-    const result = await client.query("SELECT * FROM get_user WHERE id = $1", [
+    const result = await client.query("SELECT * FROM users WHERE id = $1", [
       id,
     ]);
     const user = result.rows[0];
@@ -26,16 +26,13 @@ exports.getUserById = async (id) => {
 exports.getUserByName = async (username) => {
   try {
     const result = await client.query(
-      "SELECT * FROM get_user WHERE username LIKE $1",
+      "SELECT * FROM users WHERE username LIKE $1",
       [username]
     );
     const user = result.rows[0];
     return user;
   } catch (err) {
-    return new AppError(
-      "Database error: Unable to fetch user by name",
-      500
-    );
+    return new AppError("Database error: Unable to fetch user by name", 500);
   }
 };
 
